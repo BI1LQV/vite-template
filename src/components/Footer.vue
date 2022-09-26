d6ec1393f05a07dd89c09f5684247d1076573159
BI1LQV <bi1lqv.y@gmail.com>
2022-09-26 20:50:23 +0800

    chore: init

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { toggleDark } from "@/composables/dark"
+</script>
+
+<template>
+  <nav text-xl mt-6 inline-flex gap-2>
+    <button icon-btn @click="toggleDark()">
+      <div dark:i-carbon-moon i-carbon-sun />
+    </button>
+
+    <a
+      icon-btn i-carbon-logo-github
+      rel="noreferrer"
+      href="https://github.com/antfu/vitesse-lite"
+      target="_blank"
+      title="GitHub"
+    />
+  </nav>
+</template>
